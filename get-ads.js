61fe70a46a6038f6f7aae7192288d205a1ffe312
Klemen Slatnar <klemen@butterfly.ai>
2023-02-28 02:46:02 +0100

    Add util that fetches ads

---
+++ get-ads.js
@@ -0,0 +1,120 @@
+import { getPage, killCookieConsent, killBrowser } from './browser'
+import { Ads, knex } from './db'
+import difference from 'lodash/difference'
+
+/**
+ * 
+ * @param {import('puppeteer').Page} page 
+ */
+async function getNumberOfTotalPages(page) {
+  console.log('Searching for last page indicator')
+  let lastPage = await page
+    .waitForSelector('.paging_last > a', { timeout: 1000 })
+    .catch(_ => false)
+
+  let pages = 1
+  if(!lastPage) {
+    console.log('Selector not found. Assuming no pages')
+    return pages
+  } 
+  const pageContent = await lastPage.evaluate(el => el.href)
+  const matches = pageContent.match(/(\/(\d+)$|(\d+)\/\?s=16$)/)
+  if(!matches) {
+    console.log('Selector found but no matches found')
+    return pages
+  }
+
+  const [_ ,__ , val, val2] = matches
+  const value = [val, val2].filter(v => v)
+  if(!value) {
+    console.log('Matches found but no value found')
+    return pages
+  }
+
+  const lastPageNumber = Number.parseInt(value, 10)
+  console.log('Last page content', lastPageNumber)
+  return lastPageNumber
+}
+
+/**
+ * 
+ * @param {import('puppeteer').Page} page 
+ */
+async function extractUrls(page) {
+  console.log('Searching for adds', { url: page.url()})
+  const ads = await page.$$('div[itemprop=item] > h2 > a')
+  const links = await Promise.all(ads.map(ad => ad.evaluate(el => el.href)))
+  if(!links.length) {
+    console.log('No links found on page')
+    return false
+  }
+  const existingAddLinks = await Ads().whereIn('link', links)
+    .then(ads => ads.map(ad => ad.link))
+
+  const newLinks = difference(links, existingAddLinks)
+  if(!newLinks.length) {
+    console.log('No new links found')
+    return false
+  }
+
+  const rows = newLinks.reduce((memo, link) => {
+    memo.push({
+      link
+    })
+    return memo
+  }, [])
+
+  console.log('Inserting ads', { num: rows.length })
+  await knex.batchInsert('ads', rows, 100)
+
+  return true
+}
+
+async function evaluatePage(urlGenerator) {
+  const url = urlGenerator(false)
+
+  console.log('Starting url', { url })
+  const page = await getPage()
+
+  await advance(url)
+
+  const pages = await getNumberOfTotalPages(page)
+
+  console.log('In total there are', pages, 'page/s')
+
+  for(let currentPage = 1; currentPage <= pages; currentPage++) {
+    const nextPage = await extractUrls(page)
+    if(!nextPage) break;
+
+    if(currentPage < pages) {
+      await new Promise(resolve => setTimeout(resolve, 5000))
+
+      await advance(urlGenerator(currentPage + 1))
+    }
+  }
+
+  console.log('Finished with', { url })
+
+  async function advance(url) {
+    console.log('Navigating to URL')
+    await page.goto(url)
+
+    console.log('Killing cookie consent')
+    await killCookieConsent(page)
+  }
+}
+
+export async function scrape() {
+  await evaluatePage((nextPage) => {
+    if(!nextPage) return 'https://www.nepremicnine.net/oglasi-oddaja/ljubljana-okolica/stanovanje/?s=16#'
+    return `https://www.nepremicnine.net/oglasi-oddaja/ljubljana-okolica/stanovanje/${nextPage}/?s=16#`
+  })
+  await evaluatePage((nextPage) => {
+    if(!nextPage) return 'https://www.nepremicnine.net/oglasi-oddaja/ljubljana-okolica/hisa/?s=16'
+    return `https://www.nepremicnine.net/oglasi-oddaja/ljubljana-okolica/hisa/${nextPage}/?s=16`
+  })
+  await evaluatePage((nextPage) => {
+    if(!nextPage) return 'https://www.nepremicnine.net/oglasi-oddaja/ljubljana-okolica/vikend/?s=16'
+    return `https://www.nepremicnine.net/oglasi-oddaja/ljubljana-okolica/vikend/${nextPage}/?s=16`
+  })
+}
