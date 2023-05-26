1730b8ac3ec5cb030553f59df31c10723f63f49a
Klemen Slatnar <klemen@butterfly.ai>
2023-05-26 02:35:31 +0200

    Flush logs

--- scripts/util.js
+++ scripts/util.js
@@ -5,10 +5,12 @@ import { logtail } from '../utils/log'
 export function execute(fn) {
   fn().then(() => {
     logtail.log('Done')
+    logtail.flush()
     process.exit(0)
   })
   .catch(err => {
       logtail.error(err)
+      logtail.flush()
       process.exit(1)
   })
 }
