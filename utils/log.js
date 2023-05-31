79c372f5029eb8d27bcd4b0ff1aa3b0cd3d1b2d6
Klemen Slatnar <klemen@butterfly.ai>
2023-05-31 13:42:48 +0200

    Change log instance

--- utils/log.js
+++ utils/log.js
@@ -2,4 +2,40 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 import { Logtail } from '@logtail/node'
 
-export const logtail = new Logtail(process.env.LOGTAIL)
+const logtailInstance = new Logtail(process.env.LOGTAIL)
+
+const localInstance = {
+  debug: (...args) => {
+    if(!process.env.RENDER) {
+      console.debug(...args)
+    }
+  },
+  info: (...args) => {
+    if(process.env.RENDER) {
+      logtailInstance.info(...args)
+    } else {
+      console.info(...args)
+    }
+  },
+  warn: (...args) => {
+    if(process.env.RENDER) {
+      logtailInstance.warn(...args)
+    } else {
+      console.warn(...args)
+    }
+  },
+  error: (...args) => {
+    if(process.env.RENDER) {
+      logtailInstance.warn(...args)
+    } else {
+      console.warn(...args)
+    }
+  },
+  flush: () => {
+    if(process.env.RENDER) {
+      logtailInstance.flush()
+    }
+  }
+}
+
+export const logtail = localInstance
