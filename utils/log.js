16a2eb2d556849439eae30c9077371766f1dd8e7
Klemen Slatnar <klemen@butterfly.ai>
2023-05-31 14:15:13 +0200

    Add option to force local debug

--- utils/log.js
+++ utils/log.js
@@ -1,38 +1,44 @@
-import * as dotenv from 'dotenv' 
+import * as dotenv from 'dotenv'
 dotenv.config()
 import { Logtail } from '@logtail/node'
 
 const logtailInstance = new Logtail(process.env.LOGTAIL)
 
+const dumpLocal = !process.env.RENDER || process.env.FORCE_LOCAL_DEBUG
+const dumpLogtail = process.env.RENDER
+
 const localInstance = {
   debug: (...args) => {
-    if(!process.env.RENDER) {
+    if(dumpLocal) {
       console.debug(...args)
     }
   },
   info: (...args) => {
-    if(process.env.RENDER) {
+    if(dumpLogtail) {
       logtailInstance.info(...args)
-    } else {
+    }
+    if(dumpLocal) {
       console.info(...args)
     }
   },
   warn: (...args) => {
-    if(process.env.RENDER) {
+    if(dumpLogtail) {
       logtailInstance.warn(...args)
-    } else {
+    }
+    if(dumpLocal) {
       console.warn(...args)
     }
   },
   error: (...args) => {
-    if(process.env.RENDER) {
+    if(dumpLogtail) {
       logtailInstance.warn(...args)
-    } else {
+    }
+    if(dumpLocal) {
       console.warn(...args)
     }
   },
   flush: () => {
-    if(process.env.RENDER) {
+    if(dumpLogtail) {
       logtailInstance.flush()
     }
   }
